1c865445004a0fc66021a20f1d40ffb76debeda6
eddogustian <eddogustian78@gmail.com>
2020-02-24 00:03:27 +0700

    fix pos

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,17 @@
-$(document).on("click", ".topnav a", function() {
-  $(this)
-    .addClass("active")
-    .siblings()
-    .removeClass("active");
+$(document).ready(function() {
+  // tab
+  $(document).on("click", ".topnav a", function() {
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+  });
+  // tab end
+  // $(document).on("scroll", function() {
+  //   if ($(document).scrollTop() > 10) {
+  //     $(".NavFix").addClass("sticky");
+  //   } else {
+  //     $(".NavFix").removeClass("sticky");
+  //   }
+  // });
 });
